2881ffb85fe78e31e8cfc4db475da8ef822920e7
Anurag <yadananurag307@gmail.com>
2022-04-22 16:55:22 +0530

    Social Media app lecture 9

--- app/src/main/java/com/anurag/socialmedia_gfg1/ui/FeedFragment.kt
+++ app/src/main/java/com/anurag/socialmedia_gfg1/ui/FeedFragment.kt
@@ -49,9 +49,8 @@ class FeedFragment : Fragment() {
 
         val recyclerViewOptions = FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java)
 
-        adapter = context?.let {
-            FeedAdapter(recyclerViewOptions, it)
-        }
+
+
 
     }
 }
