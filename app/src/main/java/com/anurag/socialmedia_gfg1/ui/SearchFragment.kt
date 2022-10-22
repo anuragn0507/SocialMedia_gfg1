e534ed331e294218d98bfefe4f193c38e007acf4
Anurag <yadavanurag307@gmail.com>
2022-10-22 20:26:11 +0530

    social media app lecture 12 after 42 min

--- app/src/main/java/com/anurag/socialmedia_gfg1/ui/SearchFragment.kt
+++ app/src/main/java/com/anurag/socialmedia_gfg1/ui/SearchFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.anurag.socialmedia_gfg1.R
+import com.anurag.socialmedia_gfg1.adapters.SearchAdapter
 
 
 class SearchFragment : Fragment() {
-
+    private  lateinit var searchAdapter: SearchAdapter
+    private lateinit var searchRecyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
