52d7d9f8ea0a64800ff9afa95cb47f07c6fe554a
Anurag <yadavanurag307@gmail.com>
2022-10-22 23:07:55 +0530

    social media app lecture 12 after 43 min

--- app/src/main/java/com/anurag/socialmedia_gfg1/ui/SearchFragment.kt
+++ app/src/main/java/com/anurag/socialmedia_gfg1/ui/SearchFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
 import com.anurag.socialmedia_gfg1.R
 import com.anurag.socialmedia_gfg1.adapters.SearchAdapter
@@ -20,6 +21,14 @@ class SearchFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar: Toolbar= view.findViewById(R.id.search_toolbar)
+        toolbar.title = "Search Users"
+
     }
 
 
