2fc408bf6b9b506714a67971ac1b875efe58a99a
Anurag <yadavanurag307@gmail.com>
2022-10-23 09:21:43 +0530

    social media app lecture 12 after 49 min

--- app/src/main/java/com/anurag/socialmedia_gfg1/ui/SearchFragment.kt
+++ app/src/main/java/com/anurag/socialmedia_gfg1/ui/SearchFragment.kt
@@ -7,12 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
+import com.anurag.socialmedia_gfg1.MainActivity
 import com.anurag.socialmedia_gfg1.R
 import com.anurag.socialmedia_gfg1.adapters.SearchAdapter
+import com.anurag.socialmedia_gfg1.models.User
+import com.anurag.socialmedia_gfg1.utils.UserUtils
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class SearchFragment : Fragment() {
-    private  lateinit var searchAdapter: SearchAdapter
+    private  var searchAdapter: SearchAdapter?= null
     private lateinit var searchRecyclerView: RecyclerView
 
     override fun onCreateView(
@@ -29,7 +34,19 @@ class SearchFragment : Fragment() {
         val toolbar: Toolbar= view.findViewById(R.id.search_toolbar)
         toolbar.title = "Search Users"
 
+        (activity as? MainActivity)?.setSupportActionBar(toolbar)
+        (activity as? MainActivity)?.supportActionBar?.show()
+        setHasOptionsMenu(true)
+
+        val firestore = FirebaseFirestore.getInstance()
+        val query = firestore.collection("Users")
+            .whereNotEqualTo("id", UserUtils.user?.id)
+
+        val firestoreRecyclerOptions = FirestoreRecyclerOptions.Builder<User>().setQuery(query, User::class.java).build()
+        // Read about pagination
+
+       context?.let{
+           searchAdapter = SearchAdapter(firestoreRecyclerOptions, it)
+       }
     }
-
-
 }
