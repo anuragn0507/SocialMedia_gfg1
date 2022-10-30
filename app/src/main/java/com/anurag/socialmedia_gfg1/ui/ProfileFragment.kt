c28c6617186c0696bf6bab37504aad34984a4c12
Anurag <yadavanurag307@gmail.com>
2022-10-30 07:02:14 +0530

    social media app lecture 12 after 1hr 14 min

--- app/src/main/java/com/anurag/socialmedia_gfg1/ui/ProfileFragment.kt
+++ app/src/main/java/com/anurag/socialmedia_gfg1/ui/ProfileFragment.kt
@@ -1,14 +1,19 @@
 package com.anurag.socialmedia_gfg1.ui
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.anurag.socialmedia_gfg1.R
+import de.hdodenhof.circleimageview.CircleImageView
 
 
 class ProfileFragment : Fragment() {
+    private lateinit var userImage: CircleImageView
+    private var imageUri: Uri?= null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +21,13 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        userImage = view.findViewById(R.id.user_image)
+        val userName: EditText = view.findViewById(R.id.user_name)
+
     }
 
 
